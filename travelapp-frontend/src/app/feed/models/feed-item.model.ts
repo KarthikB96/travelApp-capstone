0c7ef16893420460cd1473b8a887c1b4ff67e043
KarthikB96 <balasubramanya.k@husky.neu.edu>
2020-08-08 18:11:59 -0400

    First commit

---
+++ travelapp-frontend/src/app/feed/models/feed-item.model.ts
@@ -0,0 +1,27 @@
+export interface FeedItem {
+    id: number;
+    url: string;
+    caption: string;
+    description: string;
+}
+
+export const feedItemMocks: FeedItem[] = [
+    {
+    id: 0,
+    url: '/assets/mock/xander0.jpg',
+    caption: 'New York',
+    description:'description'
+    },
+    {
+    id: 0,
+    url: '/assets/mock/xander1.jpg',
+    caption: 'New York',
+    description:'description'
+    },
+    {
+    id: 0,
+    url: '/assets/mock/xander2.jpg',
+    caption: 'New York',
+    description:'description'
+    }
+];
